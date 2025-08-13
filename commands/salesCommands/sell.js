be4052568b6846411b5e1fc38c96c3ccaaa3abf5
Serski <159945982+Serski@users.noreply.github.com>
2025-08-14 00:50:02 +0300

    Refine sell command for marketplace

--- commands/salesCommands/sell.js
+++ commands/salesCommands/sell.js
@@ -1,35 +1,40 @@
-const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
-const marketplace = require('../../marketplace'); // Importing marketplace
+const { SlashCommandBuilder } = require('discord.js');
+const marketplace = require('../../marketplace');
 
-//use marketplace.postSale to post a sale
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sell')
         .setDescription('Sell an item')
         .addStringOption((option) =>
-            option.setName('itemcode')
-                .setDescription('The item code')
+            option.setName('item')
+                .setDescription('The item code or name')
                 .setRequired(true)
         )
         .addIntegerOption((option) =>
             option.setName('quantity')
                 .setDescription('The quantity of the item')
-                .setRequired(true)
         )
         .addIntegerOption((option) =>
             option.setName('price')
                 .setDescription('The price of the item')
-                .setRequired(true)
         ),
     async execute(interaction) {
-        const itemCode = interaction.options.getString('itemcode');
-        const quantity = interaction.options.getInteger('quantity');
-        const price = interaction.options.getInteger('price');
-            let reply = await marketplace.postSale(quantity, itemCode, price, interaction.user.tag, interaction.user.id)
-            if (typeof (reply) == 'string') {
-                await interaction.reply(reply);
+        const userId = interaction.user.id;
+        const rawItem = interaction.options.getString('item');
+        const price = interaction.options.getInteger('price') ?? 0;
+        const qty = interaction.options.getInteger('quantity') ?? 1;
+
+        const res = await marketplace.postSale({ userId, rawItem, price, quantity: qty });
+
+        if (!res.ok) {
+            if (res.reason === 'not_enough') {
+                await interaction.reply({ content: `You have ${res.owned} but need ${res.needed}.`, ephemeral: true });
             } else {
-                await interaction.reply({ embeds: [reply] });
+                await interaction.reply({ content: 'Failed to list that sale.', ephemeral: true });
             }
+            return;
+        }
+
+        await interaction.reply(`Listed ${qty} × ${res.itemCode} for ${price} each on the marketplace.`);
     }
 };
